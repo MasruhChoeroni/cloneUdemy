8fe7f43d7d8d41410172f6cd0f07d5d06ed1d73e
Masruh Choeroni <masruh.choeroni.azura@gotoko.co.id>
2023-02-11 11:01:07 +0700

    1st init

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html"
+)
+
+func main() {
+	// Initialize standard Go html template engine
+	engine := html.New("./views", ".html")
+	// If you want other engine, just replace with following
+	// Create a new engine with django
+	// engine := django.New("./views", ".django")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	app.Get("/", func(c *fiber.Ctx) error {
+		// Render index template
+		return c.Render("index", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
